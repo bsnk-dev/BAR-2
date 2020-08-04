37ed5d7e4f576f91380340c9e1bdd747345c380b
bsnk-dev <admin@bsnk.dev>
2020-08-04 01:49:14 -0500

    started to add self restarting for changing certain options

--- dist/lib/index.js
+++ dist/lib/index.js
@@ -22,7 +22,7 @@ gatherer.onDataGathered(async (data) => {
     if (loopsSinceLastLogin >= Config.config.checksToRelogin) {
         var hasLoggedOn = await messenger.login();
         if (!hasLoggedOn) {
-            console.log("Cannot login to Politics and War. Please check your login information in config. P&W may also be down.");
+            console.log("Cannot login to Politics and War. Please check your login information in config. P&W may also be down. Make sure to configure it in the local web ui.");
             return;
         }
         loopsSinceLastLogin = 0;
